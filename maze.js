1103ccfde595eff7a401ac6848c4893242ae84d1
loni-mitch <lonimitchell19@gmail.com>
2017-10-20 11:46:53 -0400

    exercise 3

--- maze.js
+++ maze.js
@@ -1,17 +1,30 @@
 window.onload = function(){
 	let m = document.getElementById("boundary1")
 	let myBoundary = document.querySelectorAll(".boundary");
+	let end = document.getElementById("end");
+	let win = true;
 	m.addEventListener("mouseover",lose);
 
-	for (var i = 0; i < myBoundary.length - 1; i++ )
+	for (let i = 0; i < myBoundary.length - 1; i++ )
 		{
 			myBoundary[i].addEventListener("mouseover", function() {
-				for (var j = 0; j < myBoundary.length - 1; j++ )
+				for (let j = 0; j < myBoundary.length - 1; j++ )
 				{
 					myBoundary[j].setAttribute("class", "boundary youlose");
+					win = false;
 				}
+				alert("You Lose!");
 			});
 		}	
+
+		end.addEventListener("mouseover", function(){
+
+			if (win)
+			{
+				alert("You Win!");
+			}
+			
+		})
 };
 
 function lose(){
